842507e909fd027145fd6d127d3bdd7dc1963c84
Víctor Gómez <vgc1988@gmail.com>
2020-09-05 10:24:18 +0200

    [VGC] Added CocoaPods and Alamofire 5.2

---
+++ GoliathNationalBank/Pods/Target Support Files/Pods-GoliathNationalBank/Pods-GoliathNationalBank-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoliathNationalBank : NSObject
+@end
+@implementation PodsDummy_Pods_GoliathNationalBank
+@end
---
+++ GoliathNationalBank/Pods/Target Support Files/Pods-GoliathNationalBankUITests/Pods-GoliathNationalBankUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoliathNationalBankUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GoliathNationalBankUITests
+@end
